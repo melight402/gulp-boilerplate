e0e90b8c63fc7593ab73e34aaa2b973ee91bba24
Zerk <axzerk@gmail.com>
2017-09-22 20:40:48 +0300

    updated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,134 +1,149 @@
-'use strict';
+/*
+ * Gulp Project Boilerplate, Copyright © 2017, Alexander Repeta <axzerk@gmail.com>
+ */
 
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps'),
-  autoprefixer = require('gulp-autoprefixer'),
-  uglify = require('gulp-uglify'),
-  cssnano = require('gulp-cssnano'),
-  concat = require('gulp-concat'),
-  imagemin = require('gulp-imagemin'),
-  pngquant = require('imagemin-pngquant'),
-  browserSync = require('browser-sync'),
-  del = require('del'),
-  sassGlobImport = require('gulp-sass-glob-import'),
-  rigger = require('gulp-rigger'),
-  babel = require('gulp-babel');
 
-var paths = {
+//  ,
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano');
+const del = require('del');
+const mmq = require('gulp-merge-media-queries');
+const autoprefixer = require('gulp-autoprefixer');
+const plumber = require('gulp-plumber');
+const gulpCached = require('gulp-cached');
+const cache = require('gulp-cache');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const rigger = require('gulp-rigger');
+
+// Project paths
+const paths = {
   src: {
-    html: 'src/*.html',
-    js: 'src/js/**/*.js',
-    cssGlobalImport: 'src/sass/core/tools/',
-    css: ['src/sass/core/*.scss', 'src/sass/**/*.scss'],
-    img: 'src/img/**/*.+(png|jpg|gif|svg)',
-    fonts: 'src/fonts/**/*.*'
+    html: './src/*.html',
+    js: './src/js/**/*.js',
+    css: ['src/sass/core/*.scss', './src/sass/**/*.scss'],
+    img: './src/img/**/*.+(png|jpg|gif|svg)',
+    fonts: './src/fonts/**/*.*'
   },
-  build: {
-    html: 'build/',
-    js: 'build/js/',
-    css: 'build/css/',
-    img: 'build/img/',
-    fonts: 'build/fonts/'
+  dist: {
+    html: './dist',
+    js: './dist/js',
+    css: './dist/css',
+    img: './dist/img',
+    fonts: './dist/fonts'
   },
   watch: {
-    html: 'src/**/*.html'
+    html: './src/**/*.html'
   },
-  clean: './build'
-};
-
-var serverConfig = {
-  server: {
-    baseDir: './build'
-  },
-  host: 'localhost',
-  port: 9000,
-  logPrefix: 'NASA',
-  notify: false
+  clean: './dist'
 };
 
 // Assembling .html
-gulp.task('bundleHtml', function () {
-  gulp.src(paths.src.html)
+gulp.task('bundleHtml', () => {
+  return gulp.src(paths.src.html)
+    .pipe(gulpCached('html'))
     .pipe(rigger())
-    .pipe(gulp.dest(paths.build.html))
+    .pipe(gulp.dest(paths.dist.html))
     .pipe(browserSync.reload({stream: true}));
 });
 
 // Assembling .scss files
-gulp.task('bundleCss', function () {
+gulp.task('bundleCss', () => {
   return gulp.src(paths.src.css)
-    .pipe(sassGlobImport())
     .pipe(sourcemaps.init())
     .pipe(sass({
-      outputStyle: 'expanded',
-      includePaths: [paths.src.cssGlobalImport]
+      outputStyle: ':nested'
     }).on('error', sass.logError))
     .pipe(concat('styles.min.css'))
     .pipe(autoprefixer({
-      browsers: ['last 5 versions'],
-      cascade: true
+      browsers: ['last 5 versions', '> 1%'],
+      cascade: false
     }))
+    .pipe(mmq())
     .pipe(cssnano())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.build.css))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(gulp.dest(paths.dist.css))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // Assembling .js files
-gulp.task('bundleJs', function () {
+gulp.task('bundleJs', () => {
   return gulp.src(paths.src.js)
-    .pipe(concat('scripts.min.js'))
+    .pipe(plumber())
     .pipe(sourcemaps.init())
+    .pipe(concat('scripts.min.js'))
     .pipe(babel({
       presets: ['env']
     }))
-    .pipe(sourcemaps.write())
     .pipe(uglify())
-    .pipe(gulp.dest(paths.build.js))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dist.js))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // Optimizing images
-gulp.task('bundleImg', function () {
+gulp.task('bundleImg', () => {
   return gulp.src(paths.src.img)
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()],
-      interlaced: true
-    }))
-    .pipe(gulp.dest(paths.build.img))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(cache(imagemin([
+      imagemin.gifsicle(),
+      imagemin.jpegtran(),
+      imagemin.optipng()])))
+    .pipe(gulp.dest(paths.dist.img))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // Bundling fonts
-gulp.task('bundleFonts', function () {
+gulp.task('bundleFonts', () => {
   return gulp.src(paths.src.fonts)
-    .pipe(gulp.dest(paths.build.fonts));
+    .pipe(gulp.dest(paths.dist.fonts));
 });
 
 // Watching for changes in src files
 gulp.task('watch', function () {
-  gulp.watch(paths.watch.html, {cwd: './'}, ['bundleHtml']);
-  gulp.watch(paths.src.css, {cwd: './'}, ['bundleCss']);
-  gulp.watch(paths.src.js, {cwd: './'}, ['bundleJs']);
+  gulp.watch(paths.watch.html, ['bundleHtml']);
+  gulp.watch(paths.src.css, ['bundleCss']);
+  gulp.watch(paths.src.js, ['bundleJs']);
   gulp.watch(paths.src.img, ['bundleImg']);
   gulp.watch(paths.src.fonts, ['bundleFonts']);
 });
 
 // BrowserSync server
 gulp.task('webServer', function () {
-  browserSync(serverConfig);
+  browserSync({
+    server: {
+      baseDir: './dist'
+    },
+    host: 'localhost',
+    port: 9000,
+    logPrefix: 'NASA',
+    notify: false
+  });
 });
 
-// Cleaning build dir
-gulp.task('clean:build', function () {
+// Cleaning dist dir
+gulp.task('clean:dist', function () {
   return del.sync(paths.clean);
 });
 
-// General build task
-gulp.task('build', ['bundleHtml', 'bundleCss', 'bundleJs', 'bundleFonts', 'bundleImg']);
+// Cleaning cache
+gulp.task('clean:cache', () => {
+  return cache.clearAll();
+});
+
+// General dist task
+gulp.task('dist', ['bundleHtml', 'bundleCss', 'bundleJs', 'bundleFonts', 'bundleImg']);
 
 // Default task to run
-gulp.task('default', ['clean:build', 'build', 'webServer', 'watch']);
+gulp.task('start', ['clean:dist', 'clean:cache', 'dist', 'webServer', 'watch']);
